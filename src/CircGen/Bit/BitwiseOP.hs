0b641dfc2f9f5e3cc49bcbfbf65ae7ba6af3754c
Alex Parent <aparent@uwaterloo.ca>
2013-03-12 15:45:43 -0400

    Added some out of place bitwise operations

--- src/CircGen/Bit/BitwiseOP.hs
+++ src/CircGen/Bit/BitwiseOP.hs
@@ -33,21 +33,30 @@ bitOR a b c = do tof a b c
 bitXOR :: String -> String -> String -> CircuitState ()  
 bitXOR a b c = do cnot a c
                   cnot b c 
-
+bitwiseAND :: [String] -> [String] -> [String] ->  CircuitState ()
 bitwiseAND = bitwise bitAND
+
+bitwiseOR :: [String] -> [String] -> [String] ->  CircuitState ()
 bitwiseOR = bitwise bitOR
+
+bitwiseXOR :: [String] -> [String] -> [String] ->  CircuitState ()
 bitwiseXOR = bitwise bitXOR
 
 mkBitwise:: ([String] -> [String] -> [String] ->  CircuitState ()) ->  [String] -> [String] -> [String] -> Circuit
 mkBitwise f a b c = circ
   where (_,(_,_,circ)) = runState go ([],  ['c':show x | x <- [0 .. 4]], Circuit (LineInfo [] [] [] []) [] [])
-        go             = do initLines a
-                            initLines b 
-                            initLines c
+        go             = do _ <- initLines a
+                            _ <- initLines b 
+                            _ <- initLines c
                             f a b c
                             setOutputs $ a ++ b ++ c
 
 
+mkBitwiseAND :: [String] -> [String] -> [String] -> Circuit
 mkBitwiseAND = mkBitwise bitwiseAND
+
+mkBitwiseOR :: [String] -> [String] -> [String] -> Circuit
 mkBitwiseOR = mkBitwise bitwiseOR
+
+mkBitwiseXOR :: [String] -> [String] -> [String] -> Circuit
 mkBitwiseXOR = mkBitwise bitwiseXOR
--- src/qacg.hs
+++ src/qacg.hs
@@ -7,6 +7,7 @@ import Text.XML.HXT.Core
 import CircGen.Mult.SimpleMult
 import CircGen.Add.SimpleRipple
 import CircGen.Bit.Shift
+import CircGen.Bit.BitwiseOP
 
 --usage: qacg <directory> <circuitName> <size>
 main :: IO()
@@ -20,10 +21,19 @@ main = do
     "adderUnsignedInPlaceCarryControl" ->  writeCircuit (dir++cName)  $ simpleCtrlRippleSize size
     "multUnsignedOutOfPlace"           ->  writeCircuit (dir++cName)  $ simpleMultSize size
     "shiftLeftControl"                 ->  writeCircuit (dir++cName)  $ contShift size
+    "andOutOfPlace"                    ->  writeCircuit (dir++cName)  $ andOutOfPlace size
+    "orOutOfPlace"                     ->  writeCircuit (dir++cName)  $ orOutOfPlace size
+    "xorOutOfPlace"                    ->  writeCircuit (dir++cName)  $ xorOutOfPlace size
     _ -> putStrLn $ "No generator for " ++ cName
   where simpleRippleSize n = mkSimpleRipple ['a':show x|x<-[0..n-1]] ['b':show x|x<-[0..n-1]] "z"
         simpleCtrlRippleSize n = mkSimpleCtrlRipple "control" ['a':show x|x<-[0..n-1]] ['b':show x|x<-[0..n-1]] "z"
         simpleMultSize n = mkSimpleMult ['a':show x|x<-[0..n-1]] ['b':show x|x<-[0..n-1]]
+        andOutOfPlace n = mkBitwiseAND ['a':show x|x<-[0..n-1]] ['b':show x|x<-[0..n-1]] ['z':show x|x<-[0..n-1]]
+        orOutOfPlace  n = mkBitwiseOR  ['a':show x|x<-[0..n-1]] ['b':show x|x<-[0..n-1]] ['z':show x|x<-[0..n-1]]
+        xorOutOfPlace n = mkBitwiseXOR ['a':show x|x<-[0..n-1]] ['b':show x|x<-[0..n-1]] ['z':show x|x<-[0..n-1]]
+
+
+        
 
 writeCircuit :: String-> Circuit -> IO()
 writeCircuit fname circ = do 
